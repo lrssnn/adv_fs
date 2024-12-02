a5187d6afa6cb835a905b310fd5597466c0e8483
Niklas Larsson <niklaslarsson95@gmail.com>
2024-12-02 18:51:43 +1100

    Finalise Day 2, satisfied my broken attempt was really broken

--- Day2.fs
+++ Day2.fs
@@ -4,59 +4,31 @@ open System.IO
 open FSharpx.String
 
 type Day2() =
-    let testInput =
-        "7 6 4 2 1\n\
-         1 2 7 8 9\n\
-         9 7 6 2 1\n\
-         1 3 2 4 5\n\
-         8 6 4 4 1\n\
-         1 3 6 7 9"
-
     let input = trim (File.ReadAllText "input/input02.txt")
 
-    let reports input =
-        input
-        |> splitChar [| '\n' |]
-        |> Array.map (splitChar [| ' ' |])
-        |> Array.map (Array.map int)
+    let reports input = Input.readLinesAsInts input ' '
 
-    // TODO Figure out why this isn't enough
-    // let rec differencesWithin grace min max (report: int array) =
-    //     let parity = sign (report[1] - report[0])
-    //
-    //     report
-    //     |> Array.indexed
-    //     |> Array.pairwise
-    //     |> Array.forall (fun ((i, a), (j, b)) ->
-    //         let diff = b - a
-    //         let valid = sign diff = parity && min <= abs diff && abs diff <= max
-    //
-    //         valid
-    //         || (grace
-    //             && (differencesWithin false min max (Array.removeAt i report)
-    //                 || differencesWithin false min max (Array.removeAt j report))))
-
-    let rec differencesWithin min max (report: int array) =
+    // Validity rule on a report (a sequence of ints)
+    // All differences must be between 1 and 3, in one direction for the whole list
+    let satisfyTest (report: int list) =
         let parity = sign (report[1] - report[0])
 
         report
-        |> Array.indexed
-        |> Array.pairwise
-        |> Array.forall (fun ((i, a), (j, b)) ->
+        |> List.pairwise
+        |> List.forall (fun (a, b) ->
             let diff = b - a
-            let valid = sign diff = parity && min <= abs diff && abs diff <= max
+            sign diff = parity && 1 <= abs diff && abs diff <= 3)
 
-            valid)
-
-    let isSafe grace (report: int array) =
-        differencesWithin 1 3 report
+    // Check if the report is deemed safe. If there is grace (true), then we must try removing single item at
+    // a time, if one of those is safe, then the report itself is safe
+    let isSafe grace (report: int list) =
+        satisfyTest report
         || (grace
             && [ 0 .. report.Length - 1 ]
-               |> List.map (fun remove -> differencesWithin 1 3 (Array.removeAt remove report))
-               |> List.exists id)
+               |> List.exists (fun remove -> satisfyTest (List.removeAt remove report)))
 
-    let solve grace (reports: int array array) =
-        reports |> Array.filter (isSafe grace) |> Array.length
+    let solve grace (reports: int list list) =
+        reports |> List.filter (isSafe grace) |> List.length
 
     interface Day with
         member this.DayName = "02"
