6ebe1621fae6f695879dca040b98a4a050a72ed4
Niklas Larsson <niklaslarsson95@gmail.com>
2025-01-06 10:01:24 +1100

    Day 14, 15

--- Day14.fs
+++ Day14.fs
@@ -5,33 +5,21 @@ open Puz.Convenience
 open FSharpx.String
 
 type Vec2 =
-    { mutable x: int
-      mutable y: int }
+    { x: int
+      y: int }
 
     static member OfString input =
-        let (left, right) = splitOnceStr "," input
+        let left, right = splitOnceStr "," input
         { x = int left; y = int right }
-
-    static member wrappedAdd pos vel limits turns =
-        let mutable x = (pos.x + (turns * vel.x)) % limits.x
-        let mutable y = (pos.y + (turns * vel.y)) % limits.y
-
-        if x < 0 then
-            x <- x + (((-x / limits.x) + 1) * limits.x)
-
-        if y < 0 then
-            y <- y + (((-y / limits.y) + 1) * limits.y)
-
-        { x = x; y = y }
 
     member v.ToString = $"({v.x},{v.y})"
 
 type Guard =
-    { mutable pos: Vec2
+    { pos: Vec2
       vel: Vec2 }
 
     static member OfString input =
-        let (left, right) = splitOnceStr " v=" input
+        let left, right = splitOnceStr " v=" input
 
         { pos = Vec2.OfString left[2..]
           vel = Vec2.OfString right }
@@ -39,77 +27,106 @@ type Guard =
     member g.ToString = $"p: {g.pos.ToString} | v: {g.vel.ToString}"
     member g.isAt(x, y) = g.pos.x = x && g.pos.y = y
 
-    member g.Update limits turns =
-        g.pos <- Vec2.wrappedAdd g.pos g.vel limits turns
-
 type Day14() =
     let input = readText "input/input14.txt"
 
-    let testInput =
-        "p=0,4 v=3,-3\n\
-        p=6,3 v=-1,-3\n\
-        p=10,3 v=-1,2\n\
-        p=2,0 v=2,-1\n\
-        p=0,0 v=1,3\n\
-        p=3,0 v=-2,-2\n\
-        p=7,6 v=-1,-3\n\
-        p=3,0 v=-1,-2\n\
-        p=9,3 v=2,3\n\
-        p=7,3 v=-1,2\n\
-        p=2,4 v=2,-3\n\
-        p=9,5 v=-3,-3"
+    let categorise position dimensions =
+        let xMid = dimensions.x / 2
+        let yMid = dimensions.y / 2
+
+        let isLeft = position.x < xMid
+        let isRight = position.x > xMid
+        let isUp = position.y < yMid
+        let isDown = position.y > yMid
+
+        match (isLeft, isRight, isUp, isDown) with
+        | true, false, true, false -> 0 // Up Left
+        | false, true, true, false -> 1 // Up Right
+        | true, false, false, true -> 2 // Down Left
+        | false, true, false, true -> 3 // Down Right
+        | _ -> 4 // not in a quadrant
 
     let stateToStr limits (guards: Guard array) =
-        [ 0 .. (limits.y - 1) ]
+        [ 0 .. limits.y ]
         |> List.map (fun y ->
-            [ 0 .. (limits.x - 1) ]
+            [ 0 .. limits.x ]
             |> List.map (fun x ->
                 let g = guards |> Array.filter (fun guard -> guard.isAt (x, y)) |> Array.length
                 if g = 0 then "." else string g)
             |> String.concat "")
         |> String.concat "\n"
 
+    let hasEmptiesX limits guards threshold =
+        [ 0 .. limits.x ]
+        |> List.filter (fun x -> guards |> Array.forall (fun g -> g.pos.x <> x))
+        |> List.length > threshold
+
+    let hasEmptiesY limits guards threshold =
+        [ 0 .. limits.y ]
+        |> List.filter (fun y -> guards |> Array.forall (fun g -> g.pos.y <> y))
+        |> List.length > threshold
+
     let evaluate limits guards =
-        let xMid = limits.x / 2
-        let yMid = limits.y / 2
-
         guards
-        |> Array.countBy (fun g ->
-            let isLeft = g.pos.x <= xMid
-            let isRight = g.pos.x > xMid + 1
-            let isUp = g.pos.y <= yMid
-            let isDown = g.pos.y > yMid + 1
-
-            match (isLeft, isRight, isUp, isDown) with
-            | (true, false, true, false) -> 0 // Up Left
-            | (false, true, true, false) -> 1 // Up Right
-            | (true, false, false, true) -> 2 // Down Left
-            | (false, true, false, true) -> 3 // Down Right
-            | _ -> 4 // not in a quadrant
-        )
+        |> Array.countBy (fun g -> categorise g.pos limits)
         |> Array.filter (fun (group, _) -> not (group = 4))
         |> Array.map snd
         |> Array.fold (fun acc e -> acc * e) 1
 
+    // Stolen :)
+    let wrappedAdd v delta maxValue =
+        let m = maxValue + 1
+        let v2 = v + delta
+        let v3 = v2 + (1 - v2 / m) * m
+        v3 % m
+
+    let moveGuard guard limits turns =
+        { guard with
+            pos =
+                { x = wrappedAdd guard.pos.x (guard.vel.x * turns) limits.x
+                  y = wrappedAdd guard.pos.y (guard.vel.y * turns) limits.y } }
+
     interface Day with
         member this.DayName = "14"
-        member this.answer1 = "?"
-        member this.answer2 = "?"
+        member this.answer1 = "230436441"
+        member this.answer2 = "8270"
 
         member this.part1() =
             let guards = input |> splitChar [| '\n' |] |> Array.map Guard.OfString
-            //let lim = { x = 11; y = 7 }
-            let lim = { x = 101; y = 103 }
+            let dimensions = { x = 101; y = 103 }
 
-            // [ 1..100 ]
-            // |> List.iter (fun _turn -> guards |> Array.iter (fun g -> g.Update lim))
+            let lim =
+                { dimensions with
+                    x = dimensions.x - 1
+                    y = dimensions.y - 1 }
 
-            guards |> Array.iter (fun g -> g.Update lim 100)
+            guards |> Array.map (fun g -> moveGuard g lim 100) |> evaluate lim |> string
 
-            printfn "\n%s" (stateToStr lim guards)
+        member this.part2() =
+            let guards = input |> splitChar [| '\n' |] |> Array.map Guard.OfString
+            let dimensions = { x = 101; y = 103 }
 
-            evaluate lim guards |> string
-            // Should be > 220983048
+            let lim =
+                { dimensions with
+                    x = dimensions.x - 1
+                    y = dimensions.y - 1 }
 
+            let mutable result = -1
 
-        member this.part2() = "!"
+            // TODO there must be more functional way to find the first time we satisfy the
+            // condition, while folding...
+            [ 1..10000 ]
+            |> List.fold
+                (fun guards turn ->
+                    let newGuards = guards |> Array.map (fun g -> moveGuard g lim 1)
+                    let emptyx = hasEmptiesX lim newGuards 5
+                    let emptyy = hasEmptiesY lim newGuards 5
+
+                    if emptyx && emptyy then
+                        result <- turn
+
+                    newGuards)
+                guards
+            |> ignore
+
+            result |> string
--- Day15.fs
+++ Day15.fs
@@ -6,6 +6,8 @@ open Puz.Convenience
 type Tile =
     | Wall
     | Box
+    | BoxL
+    | BoxR
     | Space
 
     static member FromChar c =
@@ -21,6 +23,16 @@ type Tile =
         | Tile.Wall -> "#"
         | Tile.Box -> "O"
         | Tile.Space -> "."
+        | Tile.BoxL -> "["
+        | Tile.BoxR -> "]"
+
+    static member expandFromChar c =
+        match c with
+        | '#' -> [ Wall; Wall ]
+        | 'O' -> [ BoxL; BoxR ]
+        | '.' -> [ Space; Space ]
+        | '@' -> [ Space; Space ]
+        | _ -> failwith $"Invalid tile char {c}"
 
 type Coord =
     { x: int
@@ -36,30 +48,95 @@ type Coord =
         | 'v' -> { x = 0; y = 1 }
         | _ -> failwith $"Invalid movement char {c}"
 
+    member t.left = { t with x = t.x - 1 }
+    member t.right = { t with x = t.x + 1 }
+
+    member t.asMove =
+        match t with
+        | { x = 1; y = 0 } -> ">"
+        | { x = -1; y = 0 } -> "<"
+        | { x = 0; y = -1 } -> "^"
+        | { x = 0; y = 1 } -> "v"
+        | _ -> failwith $"Invalid move {t}"
+
+    member t.isVertical =
+        match t with
+        | { x = 1; y = 0 } -> false
+        | { x = -1; y = 0 } -> false
+        | { x = 0; y = -1 } -> true
+        | { x = 0; y = 1 } -> true
+        | _ -> failwith $"Invalid move {t}"
+
+    override this.ToString() = $"({this.x},{this.y})"
+
 type Day15() =
     let input = readText "input/input15.txt"
 
     let testInput =
-        "########\n\
-        #..O.O.#\n\
-        ##@.O..#\n\
-        #...O..#\n\
-        #.#.O..#\n\
-        #...O..#\n\
-        #......#\n\
-        ########\n\
+        "#######\n\
+#...#.#\n\
+#.....#\n\
+#..OO@#\n\
+#..O..#\n\
+#.....#\n\
+#######\n\
 \n\
-        <^^>>>vv<v>>v<<"
+<vv<<^^<<^^"
+
+    let testInput1 =
+        "########\n\
+#..O.O.#\n\
+##@.O..#\n\
+#...O..#\n\
+#.#.O..#\n\
+#...O..#\n\
+#......#\n\
+########\n\
+\n\
+<^^>>>vv<v>>v<<"
+
+    let testInput2 =
+        "##########\n\
+#..O..O.O#\n\
+#......O.#\n\
+#.OO..O.O#\n\
+#..O@..O.#\n\
+#O#..O...#\n\
+#O..O..O.#\n\
+#.OO.O.OO#\n\
+#....O...#\n\
+##########\n\
+\n\
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^\n\
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v\n\
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<\n\
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^\n\
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><\n\
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^\n\
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^\n\
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>\n\
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>\n\
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
+
+    let findRobot input =
+        let lines = input |> readLines
+        let y = lines |> Array.findIndex (String.exists (fun c -> c = '@'))
+        let x = lines[y] |> Seq.findIndex (fun c -> c = '@')
+        { x = x; y = y }
 
     let parseMap input =
         let tiles = input |> readAsCharA2D |> Array2D.map Tile.FromChar
-        let lines = input |> readLines
+        tiles, (findRobot input)
 
-        let y = lines |> Array.findIndex (String.exists (fun c -> c = '@'))
+    let expandMap input =
+        let tiles =
+            input
+            |> readLines
+            |> Array.map (fun line -> line |> Seq.collect Tile.expandFromChar)
+            |> array2D
 
-        let x = lines[y] |> Seq.findIndex (fun c -> c = '@')
-
-        tiles, { x = x; y = y }
+        let loc = findRobot input
+        tiles, { loc with x = loc.x * 2 }
 
     let parseSeq input =
         input |> Seq.filter (fun c -> not (c = '\n')) |> Seq.map Coord.FromChar
@@ -74,39 +151,106 @@ type Day15() =
         |> List.map (String.concat "")
         |> String.concat "\n"
 
-    let parse input =
+    let parse input expand =
         let mapS, sequenceS = splitOnceStr "\n\n" input
-        let map, robot = parseMap mapS
+        let map, robot = if expand then expandMap mapS else parseMap mapS
         let sequence = parseSeq sequenceS
 
         map, sequence, robot
 
-    let rec tryFindSpace (tiles: Tile array2d) from move =
+    let rec tryFindSpace (tiles: Tile array2d) from (move: Coord) =
+        //printfn $"tryFindSpace {from} {move.asMove}"
         let nextLoc = from + move
 
         match tiles[nextLoc.y, nextLoc.x] with
-        | Space -> Some nextLoc // Found
-        | Wall -> None // Cannot move
-        | Box -> tryFindSpace tiles nextLoc move // Indeterminate, need to try again
+        | Space ->
+            //printfn "  Space, good"
+            true // Found
+        | Wall ->
+            //printfn "  Wall, bad"
+            false // Cannot move
+        | BoxL when move.isVertical ->
+            //printfn $"BoxL, need to check {nextLoc} and {nextLoc.right}"
+            (tryFindSpace tiles nextLoc move) && (tryFindSpace tiles nextLoc.right move)
+        | BoxR when move.isVertical ->
+            //printfn $"BoxR, need to check {nextLoc} and {nextLoc.left}"
+            (tryFindSpace tiles nextLoc move) && (tryFindSpace tiles nextLoc.left move)
+        | _ ->
+            //printfn $"Need to check {nextLoc}"
+            tryFindSpace tiles nextLoc move // Indeterminate, need to try again
+
+    let rec applyMove (tiles: Tile array2d) from (move: Coord) =
+        //printfn $"applyMove {from} {move.asMove}"
+        let nextLoc = from + move
+
+        match tiles[nextLoc.y, nextLoc.x] with
+        | Wall -> () // Cannot move
+        | Space -> // Can move
+            tiles[nextLoc.y, nextLoc.x] <- tiles[from.y, from.x]
+            tiles[from.y, from.x] <- Tile.Space
+        | BoxL when move.isVertical ->
+            applyMove tiles nextLoc move
+            tiles[nextLoc.y, nextLoc.x] <- tiles[from.y, from.x]
+            tiles[from.y, from.x] <- Tile.Space
+            // Move the counterpart box
+            applyMove tiles nextLoc.right move
+        | BoxR when move.isVertical -> // Move the box blocking us, then move our box
+            applyMove tiles nextLoc move
+            tiles[nextLoc.y, nextLoc.x] <- tiles[from.y, from.x]
+            tiles[from.y, from.x] <- Tile.Space
+            // Move the counterpart box
+            applyMove tiles nextLoc.left move
+        | _ ->
+            applyMove tiles nextLoc move
+            tiles[nextLoc.y, nextLoc.x] <- tiles[from.y, from.x]
+            tiles[from.y, from.x] <- Tile.Space
 
     let processMove (tiles: Tile array2d) (robot: Coord) (move: Coord) =
+        //printfn $"processMove {robot} {move.asMove}"
         let nextLoc = robot + move
         let nextTile = tiles[nextLoc.y, nextLoc.x]
 
         match nextTile with
         | Space -> tiles, nextLoc // Just move
         | Wall -> tiles, robot // Can't do anything
-        | Box ->
-            // Determine if we can move the box by finding a space to push the stack into
-            match tryFindSpace tiles nextLoc move with
-            | None -> tiles, robot // Can't move
-            | Some(toMoveLoc) ->
-                tiles[nextLoc.y, nextLoc.x] <- Tile.Space
-                tiles[toMoveLoc.y, toMoveLoc.x] <- Tile.Box
+        | BoxR when move.isVertical ->
+            //printfn $"Its a box R, checking if we can move it?"
+            // Similar to box, but need to check if we can move the left box as well,
+            // And then commit with a recursive push to preserve order
+            if (tryFindSpace tiles nextLoc move) && (tryFindSpace tiles nextLoc.left move) then
+                //printfn $"Yes we can... moving {nextLoc} {move.asMove}"
+                applyMove tiles nextLoc move
+                //printfn $"moving {nextLoc.left} {move.asMove}"
+                applyMove tiles nextLoc.left move
                 tiles, nextLoc
+            else
+                tiles, robot
+        | BoxL when move.isVertical ->
+            if (tryFindSpace tiles nextLoc move) && (tryFindSpace tiles nextLoc.right move) then
+                applyMove tiles nextLoc move
+                applyMove tiles nextLoc.right move
+                tiles, nextLoc
+            else
+                tiles, robot
+        | _ ->
+            // Determine if we can move the box by finding a space to push the stack into
+            if tryFindSpace tiles nextLoc move then
+                //printfn $"Can move, moving"
+                applyMove tiles nextLoc move
+                tiles, nextLoc
+            else
+                //printfn $"Can't move"
+                tiles, robot
 
     let rec processMoves tiles robot moves =
-        moves |> Seq.fold (fun (tiles, robot) -> processMove tiles robot) (tiles, robot)
+        moves
+        |> Seq.fold
+            (fun (tiles, robot) ->
+                fun move ->
+                    let t, r = processMove tiles robot move
+                    //printfn $"{toString t r}"
+                    t, r)
+            (tiles, robot)
     // if Seq.isEmpty moves then
     //     //printfn $"Final state:"
     //     //printfn $"{toString tiles robot}"
@@ -121,6 +265,7 @@ type Day15() =
         tiles
         |> Array2D.mapi (fun y x tile ->
             match tile with
+            | Tile.BoxL
             | Tile.Box -> 100 * y + x
             | _ -> 0)
         |> Seq.cast<int>
@@ -129,11 +274,14 @@ type Day15() =
     interface Day with
         member this.DayName = "15"
         member this.answer1 = "1441031"
-        member this.answer2 = "?"
+        member this.answer2 = "1425169"
 
         member this.part1() =
-            let map, moves, robot = parse input
+            let map, moves, robot = parse input false
             let nextMap, nextRobot = processMoves map robot moves
             scoreTiles nextMap |> string
 
-        member this.part2() = "!"
+        member this.part2() =
+            let map, moves, robot = parse input true
+            let nextMap, nextRobot = processMoves map robot moves
+            scoreTiles nextMap |> string
